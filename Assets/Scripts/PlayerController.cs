a0cca22a5aa775d2fa6a028a961382ef28a71086
a57654 <a57654@alunos.ipb.pt>
2025-05-20 15:46:06 +0100

    Lesson 1.3
    Velocidade do Carro ajustada
    Adicionamos Movimento da Camera
    Mudamos o TintColor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,7 +14,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Criar movimento do Carro
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
     }
 }
